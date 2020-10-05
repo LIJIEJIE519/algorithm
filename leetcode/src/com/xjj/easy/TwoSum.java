9ba3b32561e48595abc731b8e5509bad70d31542
lijie519 <991866448@qq.com>
2020-10-05 19:33:03 +0800

    leetcode01

---
+++ leetcode/src/com/xjj/easy/TwoSum.java
@@ -0,0 +1,23 @@
+package com.xjj.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    /*
+     * 1. 使用hashmap:查找元素为O(1)
+     * 2. b = tar-a is in nums
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            // map包含减值，获取val[i]
+            if(map.containsKey(target - nums[i]))
+                return new int[] {map.get(target-nums[i]), i};
+            // 关键步，将数组存入map
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
